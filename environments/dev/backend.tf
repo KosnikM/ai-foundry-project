ed7088c2baa21ace71e9b46605583c5cc445f6e3
KosnikM <mkosnikk@gmail.com>
2026-07-07 20:47:33 +0200

    fix: update backend storage account

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstatemkai"
+    storage_account_name = "sttfstatemkproject2"
     container_name       = "tfstate"
     key                  = "dev.tfstate"
   }
