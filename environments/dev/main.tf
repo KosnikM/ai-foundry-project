41a7089cc26795cf1ce10062980c792c40611f5c
KosnikM <mkosnikk@gmail.com>
2026-07-12 13:03:39 +0200

    feat: add networking module with hub-spoke VNets and NSG rules

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -33,3 +33,301 @@ resource "azurerm_resource_group" "ai" {
   location = "swedencentral"
   tags     = local.default_tags
 }
+
+module "networking" {
+  source              = "../../modules/networking"
+  resource_group_name = azurerm_resource_group.networking.name
+  location            = local.location
+  tags                = local.default_tags
+  vnets = {
+    hub = {
+      address_space = ["10.0.0.0/16"]
+      subnets = {
+        AzureFirewallSubnet = { 
+          address_prefix = "10.0.1.0/26"
+          nsg_rules = [] 
+        }
+        AzureBastionSubnet = {
+          address_prefix = "10.0.2.0/26"
+          nsg_rules = [
+            {
+              name                       = "allow-https-inbound"
+              priority                   = 100
+              direction                  = "Inbound"
+              access                     = "Allow"
+              protocol                   = "Tcp"
+              source_port_range          = "*"
+              destination_port_range     = "443"
+              source_address_prefix      = "Internet"
+              destination_address_prefix = "*"
+            },
+            {
+              name                       = "allow-gateway-manager"
+              priority                   = 110
+              direction                  = "Inbound"
+              access                     = "Allow"
+              protocol                   = "Tcp"
+              source_port_range          = "*"
+              destination_port_range     = "443"
+              source_address_prefix      = "GatewayManager"
+              destination_address_prefix = "*"
+            },
+            {
+              name                       = "allow-azure-load-balancer"
+              priority                   = 120
+              direction                  = "Inbound"
+              access                     = "Allow"
+              protocol                   = "Tcp"
+              source_port_range          = "*"
+              destination_port_range     = "443"
+              source_address_prefix      = "AzureLoadBalancer"
+              destination_address_prefix = "*"
+            },
+            {
+              name                       = "allow-bastion-communication-5701"
+              priority                   = 131
+              direction                  = "Inbound"
+              access                     = "Allow"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "5701"
+              source_address_prefix      = "VirtualNetwork"
+              destination_address_prefix = "VirtualNetwork"
+            },
+            {
+              name                       = "allow-bastion-communication-8080"
+              priority                   = 130
+              direction                  = "Inbound"
+              access                     = "Allow"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "8080"
+              source_address_prefix      = "VirtualNetwork"
+              destination_address_prefix = "VirtualNetwork"
+            },
+            {
+              name                       = "deny-all-inbound"
+              priority                   = 4096
+              direction                  = "Inbound"
+              access                     = "Deny"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "*"
+              source_address_prefix      = "*"
+              destination_address_prefix = "*"
+            },
+            {
+              name                       = "allow-https-outbound"
+              priority                   = 100
+              direction                  = "Outbound"
+              access                     = "Allow"
+              protocol                   = "Tcp"
+              source_port_range          = "*"
+              destination_port_range     = "443"
+              source_address_prefix      = "*"
+              destination_address_prefix = "Internet"
+            },
+            {
+              name                       = "allow-ssh-rdp-outbound-22"
+              priority                   = 110
+              direction                  = "Outbound"
+              access                     = "Allow"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "22"
+              source_address_prefix      = "*"
+              destination_address_prefix = "VirtualNetwork"
+            },
+            {
+              name                       = "allow-ssh-rdp-outbound-3389"
+              priority                   = 111
+              direction                  = "Outbound"
+              access                     = "Allow"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "3389"
+              source_address_prefix      = "*"
+              destination_address_prefix = "VirtualNetwork"
+            },
+            {
+              name                       = "allow-azure-cloud-outbound"
+              priority                   = 120
+              direction                  = "Outbound"
+              access                     = "Allow"
+              protocol                   = "Tcp"
+              source_port_range          = "*"
+              destination_port_range     = "443"
+              source_address_prefix      = "*"
+              destination_address_prefix = "AzureCloud"
+            },
+            {
+              name                       = "allow-bastion-communication-outbound-8080"
+              priority                   = 130
+              direction                  = "Outbound"
+              access                     = "Allow"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "8080"
+              source_address_prefix      = "VirtualNetwork"
+              destination_address_prefix = "VirtualNetwork"
+            },
+              {
+              name                       = "allow-bastion-communication-outbound-5701"
+              priority                   = 131
+              direction                  = "Outbound"
+              access                     = "Allow"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "5701"
+              source_address_prefix      = "VirtualNetwork"
+              destination_address_prefix = "VirtualNetwork"
+            }
+        ] }
+        snet-jumpbox = { address_prefix = "10.0.3.0/29"
+          nsg_rules = [{
+            name                       = "allow-rdp-from-bastion"
+            priority                   = 100
+            direction                  = "Inbound"
+            access                     = "Allow"
+            protocol                   = "Tcp"
+            source_port_range          = "*"
+            destination_port_range     = "3389"
+            source_address_prefix      = "10.0.2.0/26"
+            destination_address_prefix = "*"
+            },
+            {
+              name                       = "deny-all-inbound"
+              priority                   = 4096
+              direction                  = "Inbound"
+              access                     = "Deny"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "*"
+              source_address_prefix      = "*"
+              destination_address_prefix = "*"
+          }]
+
+        }
+      }
+
+    }
+    spoke = {
+      address_space = ["10.1.0.0/16"]
+      subnets = {
+        snet-appgw = {
+          address_prefix = "10.1.1.0/24"
+          nsg_rules = [
+            {
+              name                       = "allow-https-inbound"
+              priority                   = 100
+              direction                  = "Inbound"
+              access                     = "Allow"
+              protocol                   = "Tcp"
+              source_port_range          = "*"
+              destination_port_range     = "443"
+              source_address_prefix      = "*"
+              destination_address_prefix = "*"
+            },
+            {
+              name                       = "allow-appgw-health-probe"
+              priority                   = 110
+              direction                  = "Inbound"
+              access                     = "Allow"
+              protocol                   = "Tcp"
+              source_port_range          = "*"
+              destination_port_range     = "65200-65535"
+              source_address_prefix      = "GatewayManager"
+              destination_address_prefix = "*"
+            },
+            {
+              name                       = "deny-all-inbound"
+              priority                   = 4096
+              direction                  = "Inbound"
+              access                     = "Deny"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "*"
+              source_address_prefix      = "*"
+              destination_address_prefix = "*"
+            }
+          ]
+        }
+        snet-app = {
+          address_prefix = "10.1.2.0/24"
+          nsg_rules = [
+            {
+              name                       = "allow-from-appgw"
+              priority                   = 100
+              direction                  = "Inbound"
+              access                     = "Allow"
+              protocol                   = "Tcp"
+              source_port_range          = "*"
+              destination_port_range     = "443"
+              source_address_prefix      = "10.1.1.0/24"
+              destination_address_prefix = "*"
+            },
+            {
+              name                       = "deny-all-inbound"
+              priority                   = 4096
+              direction                  = "Inbound"
+              access                     = "Deny"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "*"
+              source_address_prefix      = "*"
+              destination_address_prefix = "*"
+            }
+          ]
+        }
+        snet-pe = {
+          address_prefix = "10.1.3.0/24"
+          nsg_rules = [
+            {
+              name                       = "deny-all-inbound"
+              priority                   = 4096
+              direction                  = "Inbound"
+              access                     = "Deny"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "*"
+              source_address_prefix      = "*"
+              destination_address_prefix = "*"
+            }
+          ]
+        }
+      }
+    }
+    ai = {
+      address_space = ["10.2.0.0/16"]
+      subnets = {
+        snet-ai = {
+          address_prefix = "10.2.1.0/24"
+          nsg_rules = [
+            {
+              name                       = "deny-all-inbound"
+              priority                   = 4096
+              direction                  = "Inbound"
+              access                     = "Deny"
+              protocol                   = "*"
+              source_port_range          = "*"
+              destination_port_range     = "*"
+              source_address_prefix      = "*"
+              destination_address_prefix = "*"
+            }
+          ]
+        }
+      }
+    }
+  }
+  private_dns_zones = [
+    "privatelink.openai.azure.com",
+    "privatelink.vaultcore.azure.net",
+    "privatelink.blob.core.windows.net",
+    "privatelink.azurewebsites.net",
+    "privatelink.azurecr.io",
+    "privatelink.azmk8s.io",
+    "privatelink.search.azure.com",
+    "privatelink.monitor.azure.com",
+    "privatelink.azure-api.net"
+  ]
+}
--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -18,6 +18,7 @@ locals {
         vnet_key       = vnet_key
         subnet_key     = subnet_key
         address_prefix = subnet.address_prefix
+        nsg_rules      = subnet.nsg_rules
       }
     ]
   ])
@@ -31,80 +32,83 @@ resource "azurerm_subnet" "this" {
 }
 
 resource "azurerm_network_security_group" "this" {
-  for_each = { for s in local.subnets : "${s.vnet_key}-${s.subnet_key}" => s }
+  for_each            = { for s in local.subnets : "${s.vnet_key}-${s.subnet_key}" => s 
+                        if s.subnet_key != "AzureFirewallSubnet"
+  }
   name                = "${each.key}-nsg"
   location            = var.location
   resource_group_name = var.resource_group_name
-  tags = var.tags
-    dynamic "security_rule" {
-     for_each = each.value.nsg_rules
-  content {
-    name = security_rule.value.name
-    priority                   = security_rule.value.priority
-    direction                  = security_rule.value.direction
-    access                     = security_rule.value.access
-    protocol                   = security_rule.value.protocol
-    source_port_range          = security_rule.value.source_port_range
-    destination_port_range     = security_rule.value.destination_port_range
-    source_address_prefix      = security_rule.value.source_address_prefix
-    destination_address_prefix = security_rule.value.destination_address_prefix
-  }
+  tags                = var.tags
+  dynamic "security_rule" {
+    for_each = each.value.nsg_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
+      destination_address_prefix = security_rule.value.destination_address_prefix
     }
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "this" {
-  for_each = { for s in local.subnets : "${s.vnet_key}-${s.subnet_key}" => s }
+  for_each                  = { for s in local.subnets : "${s.vnet_key}-${s.subnet_key}" => s 
+    if s.subnet_key != "AzureFirewallSubnet"}
   subnet_id                 = azurerm_subnet.this[each.key].id
   network_security_group_id = azurerm_network_security_group.this[each.key].id
 }
 
 resource "azurerm_virtual_network_peering" "hub-spoke" {
-  name = "peer-hub-to-spoke"
-  resource_group_name = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this["hub"].name
+  name                      = "peer-hub-to-spoke"
+  resource_group_name       = var.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.this["hub"].name
   remote_virtual_network_id = azurerm_virtual_network.this["spoke"].id
-  allow_forwarded_traffic = true
-  allow_gateway_transit = false
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
 }
 
 resource "azurerm_virtual_network_peering" "spoke-hub" {
-  name = "peer-spoke-to-hub"
-  resource_group_name = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this["spoke"].name
+  name                      = "peer-spoke-to-hub"
+  resource_group_name       = var.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.this["spoke"].name
   remote_virtual_network_id = azurerm_virtual_network.this["hub"].id
-  allow_forwarded_traffic = true
-  allow_gateway_transit = false
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
 }
 
 resource "azurerm_virtual_network_peering" "hub-ai" {
-  name = "peer-hub-to-ai"
-  resource_group_name = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this["hub"].name
+  name                      = "peer-hub-to-ai"
+  resource_group_name       = var.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.this["hub"].name
   remote_virtual_network_id = azurerm_virtual_network.this["ai"].id
-  allow_forwarded_traffic = true
-  allow_gateway_transit = false
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
 }
 
 resource "azurerm_virtual_network_peering" "ai-hub" {
-  name = "peer-ai-to-hub"
-  resource_group_name = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this["ai"].name
+  name                      = "peer-ai-to-hub"
+  resource_group_name       = var.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.this["ai"].name
   remote_virtual_network_id = azurerm_virtual_network.this["hub"].id
-  allow_forwarded_traffic = true
-  allow_gateway_transit = false
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
 }
 
 resource "azurerm_private_dns_zone" "this" {
-    for_each = toset(var.private_dns_zones)
-    name = each.key
+  for_each            = toset(var.private_dns_zones)
+  name                = each.key
   resource_group_name = var.resource_group_name
-  tags = var.tags
+  tags                = var.tags
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
-  for_each = toset(var.private_dns_zones)  
-  name = "link-${each.key}"
-  resource_group_name = var.resource_group_name
+  for_each              = toset(var.private_dns_zones)
+  name                  = "link-${each.key}"
+  resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.this[each.key].name
-  virtual_network_id = azurerm_virtual_network.this["hub"].id
+  virtual_network_id    = azurerm_virtual_network.this["hub"].id
 }
